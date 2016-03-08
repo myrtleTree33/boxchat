ea034f2a91cb174d4168fddfed6861f6823cfb28
myrtletree33 <me@joeltong.org>
2016-03-09 00:27:16 +0800

    added necessary templates for the timeline, to be filled in.

---
+++ boxchat-app/app/client/templates/timeline/timeline_questions/timeline_question_item/timeline_question_item.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* TimelineQuestionItem: Event Handlers */
+/*****************************************************************************/
+Template.TimelineQuestionItem.events({
+});
+
+/*****************************************************************************/
+/* TimelineQuestionItem: Helpers */
+/*****************************************************************************/
+Template.TimelineQuestionItem.helpers({
+});
+
+/*****************************************************************************/
+/* TimelineQuestionItem: Lifecycle Hooks */
+/*****************************************************************************/
+Template.TimelineQuestionItem.onCreated(function () {
+});
+
+Template.TimelineQuestionItem.onRendered(function () {
+});
+
+Template.TimelineQuestionItem.onDestroyed(function () {
+});
---
+++ boxchat-app/app/client/templates/timeline/timeline_questions/timeline_questions.js
@@ -0,0 +1,23 @@
+/*****************************************************************************/
+/* TimelineQuestions: Event Handlers */
+/*****************************************************************************/
+Template.TimelineQuestions.events({
+});
+
+/*****************************************************************************/
+/* TimelineQuestions: Helpers */
+/*****************************************************************************/
+Template.TimelineQuestions.helpers({
+});
+
+/*****************************************************************************/
+/* TimelineQuestions: Lifecycle Hooks */
+/*****************************************************************************/
+Template.TimelineQuestions.onCreated(function () {
+});
+
+Template.TimelineQuestions.onRendered(function () {
+});
+
+Template.TimelineQuestions.onDestroyed(function () {
+});
