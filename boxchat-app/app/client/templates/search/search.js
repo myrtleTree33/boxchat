0435cda07f342ac6484e758ad62697e9b4a6b265
myrtletree33 <me@joeltong.org>
2016-03-23 01:54:26 +0800

    added searchbar bubble support

--- boxchat-app/app/client/templates/search/search.js
+++ boxchat-app/app/client/templates/search/search.js
@@ -11,8 +11,7 @@ Template.Search.helpers({});
 /*****************************************************************************/
 /* Search: Lifecycle Hooks */
 /*****************************************************************************/
-Template.Search.onCreated(function() {
-});
+Template.Search.onCreated(function() {});
 
 Template.Search.onRendered(function() {
   $('#input-tags').selectize({
@@ -20,9 +19,67 @@ Template.Search.onRendered(function() {
     persist: false,
     create: function(input) {
       return {
-        value: input,
-        text: input
+        title: input
+      };
+    },
+    valueField: 'title',
+    labelField: 'title',
+    searchField: ['title', 'tags'],
+    render: {
+      item: function(item, escape) {
+        var title = escape(item.title);
+        return '<div>' +
+          (item.title ? '<span class="forum-search-title">' + title + '</span>' : '') +
+          '</div>';
+      },
+      option: function(item, escape) {
+        var title = escape(item.title);
+        return '<div>' +
+          (item.title ? '<span class="forum-search-title">' + title + '</span>' : '') +
+          '</div>';
       }
+    },
+    load: function(query, callback) {
+      var results = [];
+      var forumId = Router.current().params.id;
+      if (query.charAt(0) === '#') {
+        results = Tags.find({
+          forumId: forumId,
+          tag: {
+            $regex: '.*' + query + '.*'
+          }
+        }, {
+          sort: {
+            count: -1
+          },
+          limit: 7
+        }).fetch();
+        results = _.map(results, function(element) {
+          return {
+            title: element.tag
+          };
+        });
+
+      } else {
+        results = Questions.find({
+          forumId: forumId,
+          title: {
+            $regex: '.*' + query + '.*'
+          }
+        }, {
+          sort: {
+            count: -1
+          },
+          limit: 7
+        }).fetch();
+        results = _.map(results, function(element) {
+          return {
+            title: element.title
+          };
+        });
+      }
+
+      callback(results);
     }
   });
 });
