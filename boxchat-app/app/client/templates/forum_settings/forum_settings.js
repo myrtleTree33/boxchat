49bd1769796062712334bc737b42d6cf4e7369ef
myrtletree33 <me@joeltong.org>
2016-03-24 00:38:43 +0800

    successfully added forums page

--- boxchat-app/app/client/templates/forum_settings/forum_settings.js
+++ boxchat-app/app/client/templates/forum_settings/forum_settings.js
@@ -1,7 +1,34 @@
 /*****************************************************************************/
 /* ForumSettings: Event Handlers */
 /*****************************************************************************/
-Template.ForumSettings.events({});
+Template.ForumSettings.events({
+  'submit #form-forum-settings': function(event, template) {
+    event.preventDefault();
+    var forumId = Router.current().params.id;
+    var obj = event.target;
+    var title = obj.title.value;
+    // empty or array: S/O http://stackoverflow.com/questions/10346722/how-can-i-split-a-javascript-string-by-white-space-or-comma
+    var all = obj.users.value.split(/[ ,]+/).filter(Boolean);
+    var admin = obj.admin.value.split(/[ ,]+/).filter(Boolean);
+    admin = lodash.union(admin, [Meteor.userId()]) // user should always be admin of his own forum
+    all = lodash.union(all, admin); // add all admin in as well
+    var students = lodash.pull(all, admin);
+    var description = obj.description.value;
+    var tags = obj.topics.value.split(',');
+
+    Forums.update({
+      _id: forumId
+    }, {$set: {
+      title: title,
+      description: description,
+      tags: tags,
+      all: all,
+      admin: admin,
+      students: students
+    }});
+
+  }
+});
 
 /*****************************************************************************/
 /* ForumSettings: Helpers */
@@ -26,51 +53,55 @@ Template.ForumSettings.helpers({
 Template.ForumSettings.onCreated(function() {});
 
 Template.ForumSettings.onRendered(function() {
-  var select = $('#forum-users').selectize({
-    delimiter: ',',
-    persist: false,
-    create: function(input) {
-      return {
-        'name': input
-      };
-    },
-    valueField: '_id',
-    labelField: 'name',
-    searchField: ['name'],
-    render: {
-      item: function(item, escape) {
-        var name = escape(item.name);
-        return '<div>' +
-          ('<span class="forum-search-title">' + name + '</span>') +
-          '</div>';
+  var setupUsersField = function(selector, field) {
+    var select = $(selector).selectize({
+      plugins: {
+        'remove_button': {}
       },
-      option: function(item, escape) {
-        var name = escape(item.name);
-        return '<div>' +
-          ('<span class="forum-search-title">' + name + '</span>') +
-          '</div>';
-      }
-    },
-    load: function(query, callback) {
-      var results = Meteor.users.find({
-        'profile.name': {
-          $regex: '.*' + query + '.*',
-          $options: 'i'
-        }
-      }, {
-        limit: 7
-      }).fetch();
-      var _results = _.map(results, function(user) {
+      delimiter: ',',
+      persist: false,
+      create: function(input) {
         return {
-          _id: user._id,
-          name: user.profile['name']
+          'name': input
+        };
+      },
+      valueField: '_id',
+      labelField: 'name',
+      searchField: ['name'],
+      render: {
+        item: function(item, escape) {
+          var name = escape(item.name);
+          return '<div>' +
+            ('<span class="forum-search-title">' + name + '</span>') +
+            '</div>';
+        },
+        option: function(item, escape) {
+          var name = escape(item.name);
+          return '<div>' +
+            ('<span class="forum-search-title">' + name + '</span>') +
+            '</div>';
         }
-      });
-      callback(_results);
-    }
-  });
+      },
+      load: function(query, callback) {
+        var results = Meteor.users.find({
+          'profile.name': {
+            $regex: '.*' + query + '.*',
+            $options: 'i'
+          }
+        }, {
+          limit: 7
+        }).fetch();
+        var _results = _.map(results, function(user) {
+          return {
+            _id: user._id,
+            name: user.profile['name']
+          }
+        });
+        callback(_results);
+      }
+    });
 
-    var userIds = Template.instance().data.all;
+    var userIds = Template.instance().data[field];
     var users = Meteor.users.find({
       _id: {
         $in: userIds
@@ -89,8 +120,10 @@ Template.ForumSettings.onRendered(function() {
       sselect.refreshOptions();
       sselect.addItem(item._id, false);
     });
-
-
+    return sselect; // return instance
+  };
+  setupUsersField('#forum-users', 'all');
+  setupUsersField('#forum-admin', 'admin');
 });
 
 Template.ForumSettings.onDestroyed(function() {});
