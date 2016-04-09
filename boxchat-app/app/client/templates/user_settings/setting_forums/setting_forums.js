969ecc6f02c4a4e5eac8e5a5df9210c9e25a114e
Joel Tong <me@joeltong.org>
2016-04-09 14:26:38 +0000

    pushing forum stuff

--- boxchat-app/app/client/templates/user_settings/setting_forums/setting_forums.js
+++ boxchat-app/app/client/templates/user_settings/setting_forums/setting_forums.js
@@ -1,23 +1,37 @@
 /*****************************************************************************/
 /* SettingForums: Event Handlers */
 /*****************************************************************************/
-Template.SettingForums.events({
-});
+Template.SettingForums.events({});
 
 /*****************************************************************************/
 /* SettingForums: Helpers */
 /*****************************************************************************/
 Template.SettingForums.helpers({
+    forumNum: function() {
+        var length = Forums.find({
+            all: Meteor.user()._id
+        }).count();
+        return length > 0 ? length : false;
+    },
+
+
+
+    forums: function() {
+        return Forums.find({
+            all: Meteor.user()._id
+        }, {
+            sort: {
+                createdAt: -1
+            }
+        });
+    }
 });
 
 /*****************************************************************************/
 /* SettingForums: Lifecycle Hooks */
 /*****************************************************************************/
-Template.SettingForums.onCreated(function () {
-});
+Template.SettingForums.onCreated(function() {});
 
-Template.SettingForums.onRendered(function () {
-});
+Template.SettingForums.onRendered(function() {});
 
-Template.SettingForums.onDestroyed(function () {
-});
+Template.SettingForums.onDestroyed(function() {});
