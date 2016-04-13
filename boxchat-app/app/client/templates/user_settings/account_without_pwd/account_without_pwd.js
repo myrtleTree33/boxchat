40829182a76db22e5b3f6c4a0b4dc957894232e5
Joel Tong <me@joeltong.org>
2016-04-13 17:54:15 +0000

    Able to update user name

--- boxchat-app/app/client/templates/user_settings/account_without_pwd/account_without_pwd.js
+++ boxchat-app/app/client/templates/user_settings/account_without_pwd/account_without_pwd.js
@@ -2,6 +2,17 @@
 /* AccountWithoutPwd: Event Handlers */
 /*****************************************************************************/
 Template.AccountWithoutPwd.events({
+    'submit .update_userName'(event) {
+        event.preventDefault();
+        const target = event.target;
+        const newName = target.username.value; 
+        if (newName != null && newName != "") {
+            Meteor.users.update(Meteor.userId(), {$set: {"profile.name":newName}});
+            Bert.alert('Your user name has been changed successfully', 'success', 'growl-top-right');
+        } else {
+            Bert.alert('Please enter a user name', 'warning', 'growl-top-right');
+        }
+    }
 });
 
 /*****************************************************************************/
