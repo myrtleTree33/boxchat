3036b91bc4d9b6142135abe4f9a401586c285e0b
myrtletree33 <me@joeltong.org>
2016-04-13 20:38:33 +0800

    added email changing option

--- boxchat-app/app/client/templates/user_settings/account_without_pwd/account_without_pwd.js
+++ boxchat-app/app/client/templates/user_settings/account_without_pwd/account_without_pwd.js
@@ -2,22 +2,38 @@
 /* AccountWithoutPwd: Event Handlers */
 /*****************************************************************************/
 Template.AccountWithoutPwd.events({
+  'submit #account-without-pwd-form': function(event, template) {
+    event.preventDefault();
+    var obj = event.target;
+    var email = obj.nusEmail.value;
+
+    Meteor.call('signup/addEmail', email, function(err) {
+      if (err) {
+        Bert.alert('Invalid email, please use your NUS email', 'danger', 'growl-top-right');
+      } else {
+        Meteor.call('signup/sendVerificationEmail');
+        Bert.alert('Successfully added your NUS email: ' + email + ".  Please verify your email to continue.", 'success', 'growl-top-right');
+
+        Router.go('main');
+      }
+    });
+  }
 });
 
 /*****************************************************************************/
 /* AccountWithoutPwd: Helpers */
 /*****************************************************************************/
 Template.AccountWithoutPwd.helpers({
+  userEmail: function() {
+    return Meteor.user().emails[0].address;
+  }
 });
 
 /*****************************************************************************/
 /* AccountWithoutPwd: Lifecycle Hooks */
 /*****************************************************************************/
-Template.AccountWithoutPwd.onCreated(function () {
-});
+Template.AccountWithoutPwd.onCreated(function() {});
 
-Template.AccountWithoutPwd.onRendered(function () {
-});
+Template.AccountWithoutPwd.onRendered(function() {});
 
-Template.AccountWithoutPwd.onDestroyed(function () {
-});
+Template.AccountWithoutPwd.onDestroyed(function() {});
